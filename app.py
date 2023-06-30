e74a7e4345cb7e5f2d7bccdfce063cf092f88e7d
nehakhedkar4 <nehakhedkar30@gmail.com>
2023-06-30 10:44:00 +0530

    add run.py

--- app.py
+++ app.py
@@ -2,35 +2,37 @@ from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
-app = Flask(__name__)
+def create_app():
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://admin:0000@localhost/testflask'
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+    app = Flask(__name__)
 
-class Test(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), nullable=False)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://admin:0000@localhost/testflask'
+    db = SQLAlchemy(app)
+    migrate = Migrate(app, db)
 
-    def __init__(self, name):
-        self.name = name
+    class Test(db.Model):
+        id = db.Column(db.Integer, primary_key=True)
+        name = db.Column(db.String(80), nullable=False)
 
-@app.route('/')
-def index():
-    return 'Hello, World!'
+        def __init__(self, name):
+            self.name = name
 
-@app.route('/about')
-def about():
-    return 'About page'
+    @app.route('/')
+    def index():
+        return 'Hello, World!'
 
-@app.route('/hello', methods=["GET", "POST"])
-@app.route('/hello/<name>')
-def hello(name=None):
-    if request.method == 'POST':
-        name = request.form.get('name')
-        print('name: ', name)
-        new_user = Test(name=name)
-        db.session.add(new_user)
-        db.session.commit()
+    @app.route('/about')
+    def about():
+        return 'About page'
 
-    return render_template('index.html', name=name)
+    @app.route('/hello', methods=["GET", "POST"])
+    @app.route('/hello/<name>')
+    def hello(name=None):
+        if request.method == 'POST':
+            name = request.form.get('name')
+            print('name: ', name)
+            new_user = Test(name=name)
+            db.session.add(new_user)
+            db.session.commit()
+
+        return render_template('index.html', name=name)
