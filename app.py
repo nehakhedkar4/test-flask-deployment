9448b7f37748fbce04d3b236bbb0092475f82816
nehakhedkar4 <nehakhedkar30@gmail.com>
2023-06-30 10:05:05 +0530

    add DB

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://admin:0000@localhost/testflask'
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+class Test(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+
+    def __init__(self, name):
+        self.name = name
 
 @app.route('/')
 def index():
@@ -11,7 +23,14 @@ def index():
 def about():
     return 'About page'
 
-@app.route('/hello')
+@app.route('/hello', methods=["GET", "POST"])
 @app.route('/hello/<name>')
 def hello(name=None):
+    if request.method == 'POST':
+        name = request.form.get('name')
+        print('name: ', name)
+        new_user = Test(name=name)
+        db.session.add(new_user)
+        db.session.commit()
+
     return render_template('index.html', name=name)
